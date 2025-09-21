a7d450d52580cd8b219b9357920c47ac2a168d71
Daniel Marandola <danielmarandola@tu-dominio.local>
2025-09-21 13:23:28 -0300

    fix(orders): prioriza /orders/search sobre /orders/:id reordenando controllers

--- backend/api/src/orders/orders.module.ts
+++ backend/api/src/orders/orders.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { OrdersController } from './orders.controller';
+import { OrdersDetailController } from './orders.detail.controller';
 import { OrdersQueryController } from './orders.query.controller';
 
 @Module({
-  controllers: [OrdersController, OrdersQueryController],
+  controllers: [OrdersQueryController, OrdersController, OrdersDetailController],
   providers: [PrismaService],
-  exports: [],
 })
 export class OrdersModule {}
